fb8782a0c6541d3d716f316e149941203ae5ac0f
ica-alexandra <alexandra_claudiaica@yahoo.com>
2017-04-26 16:05:19 +0300

    Login/TransactionModels

---
+++ AEAEBank/AEAEBank/Models/TransactionModel.cs
@@ -0,0 +1,37 @@
+﻿using AEAEBank.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AEAEBank.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "My Account(IBAN)")]
+        public string SourceIBAN { get; set; }
+
+
+        [Required]
+        [Display(Name = "IBAN Beneficiary")]
+        public string DestinationIBAN { get; set; }
+
+
+        [Required]
+        [Display(Name = "Transaction Type")]
+        public TransactionType TType { get; set; }
+
+
+        [Display(Name = "Amount")]
+        public decimal Amount { get; set; }
+
+
+        [Display(Name = "Status")]
+        public TransactionStatus Status { get; set; }
+    }
+}
